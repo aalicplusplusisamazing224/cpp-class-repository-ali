aa046b5f7fa705aaf37049894e7ea443df67c737
aalicplusplusisamazing224 <aali224@student.cscc.edu>
2026-02-01 19:29:07 -0500

    commit the rest of the files

---
+++ lab2.cpp
@@ -0,0 +1,22 @@
+// @file lab2 
+// @author Abdulahi Ali
+// @date 2026-02-01
+// @brief This program calculates the perimeter of a rectangle from user input. 
+
+    #include <iostream>
+    using namespace std;
+
+    int main() {
+        double length, width, perimeter;
+
+        double length, width, perimeter;
+
+        cout << "This application will calculate the perimeter of a rectangle." << endl;
+        cout << "Please enter the length of the rectangle: ";
+        cin >> length;
+        cout << "Please enter the width of the rectangle: "; 
+        cin >> width;
+        perimeter = 2 * (length + width);
+        cout << "The perimeter of the rectangle is: " << perimeter << endl;
+        return 0;
+    }
