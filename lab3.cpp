e979244dcf1ec0829190a0d5179168afa6e051e8
aalicplusplusisamazing224 <aali224@student.cscc.edu>
2026-02-08 15:45:42 -0500

    feat(lab3): Menu selection based project to calculate areas or rectangles and circles

---
+++ lab3.cpp
@@ -0,0 +1,48 @@
+// @file Lab3.cpp
+// @author Abdulahi Ali
+// @date 2025-02-20
+// @brief This program is a menu driven program to calculate areas of rectangles and circles.
+
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+    const double PI = 3.14159; 
+
+    int menuChoice;
+    double length, width, radius, area;
+
+    cout << "Menu:" << endl;
+    cout << "1. Calculate area of a rectangle" << endl;
+    cout << "2. Calculate area of a circle" << endl;
+    cout << "3. Quit" << endl;
+    cout << "Please make a selection: ";
+    cin >> menuChoice;
+
+    if (menuChoice == 1)
+    {
+        cout << "Enter the length of the rectangle: ";
+        cin >> length;
+        cout << "Enter the width of the rectangle: ";
+        cin >> width;
+        area = length * width;
+        cout << "The area of the rectangle is: " << area << endl;
+    }
+    else if (menuChoice == 2)
+    {
+        cout << "Enter the radius of the circle: ";
+        cin >> radius;
+        area = PI * radius * radius;
+        cout << "The area of the circle is: " << area << endl;
+    }
+    else if (menuChoice == 3)
+    {
+        cout << "Exiting the program. Goodbye!" << endl;
+    }
+    else
+    {
+        cout << "Invalid selection. Please try again." << endl;
+    }
+    return 0;
+ }
